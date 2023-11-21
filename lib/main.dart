52e77c2c4295711de116b26b729e1fd5dc47e283
TEDI RAHARJA <146830652+Syuted@users.noreply.github.com>
2023-11-21 11:44:51 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Space News App',
+      title: 'Latihan Responsi',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
